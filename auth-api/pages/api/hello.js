fab36f2a7aaea9a4f2c1fc99fba47a605b4a8f35
Onder Erguven <ondererguven@gmail.com>
2023-12-23 22:17:15 +0100

    revert query param code

--- auth-api/pages/api/hello.js
+++ auth-api/pages/api/hello.js
@@ -1,18 +1,3 @@
 export default function handler(req, res) {
-  const fragment = req.url.split("#")[1];
-  const urlParams = new URLSearchParams(fragment);
-
-  for (const [key, value] of urlParams) {
-    res.url.searchParams.set(key, value);
-  }
-
-
-  res.redirect(307, 'backlogslasher://oauth2redirect');
-
-
-
-
-
-
-  // res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' })
 }
--- auth-api/pages/api/twitchauth.js
+++ auth-api/pages/api/twitchauth.js
@@ -1,15 +1,3 @@
 export default function handler(req, res) {
-    // const fragment = req.url.split("#")[1];
-    // const urlParams = new URLSearchParams(fragment);
-    // const entries = urlParams.entries();
-    // res.redirect(307, 'backlogslasher://oauth2redirect')
-
-    const fragment = req.url.split("#")[1];
-    const urlParams = new URLSearchParams(fragment);
-
-    for (const [key, value] of urlParams) {
-        res.url.searchParams.set(key, value);
-    }
-
     res.redirect(307, 'backlogslasher://oauth2redirect');
 }
